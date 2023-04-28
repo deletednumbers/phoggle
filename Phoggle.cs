68adb3aa124fda949074cbf8871c845a942d017b
J.D. Satlin <jdstalin@gmail.com>
2023-04-28 14:51:57 -0700

    renamed variabbles to adhere to standards, recommended by VS

--- Phoggle.cs
+++ Phoggle.cs
@@ -7,24 +7,15 @@ class Phoggle
 {
 	// See https://aka.ms/new-console-template for more information
 
-
-
-	//h c a t w e
-	//pre populate a list of acceptable words
-	//give the user a time limit to enter as many words as they can
-	//compare the results
-
-	//collections
-	//collection of acceptable words (pass logic filter)
-	//collection of user words
-	//timers and time limits
-	static bool isTheGameBeingPlayed = true;
+	//to do
+	//find words on board and add to _gameWordsToGuessList
+	static bool _isTheGameBeingPlayed = true;
 	private static System.Timers.Timer aTimer;
-	private static List<string> playerInputList = new List<string>();
-	private static List<string> gameWordsToGuessList = new List<string> { "the guess", "WHAT", "CAT", "HAT" };
-	private static List<string> gameWordScoreList = new List<string>(gameWordsToGuessList);
-	private static int playerScore = 0;
-	private static List<string> aDie = new List<string> { "E", "C", "T", "A", "H", "W" };
+	private static List<string> _playerInputList = new List<string>();
+	private static List<string> _gameWordsToGuessList = new List<string> { "the guess", "WHAT", "CAT", "HAT" };
+	private static List<string> _gameWordScoreList = new List<string>(_gameWordsToGuessList);
+	private static int _playerScore = 0;
+	private static List<string> _aDie = new List<string> { "E", "C", "T", "A", "H", "W" };
 	static void Main(string[] args)
 	{
 		Random rnd = new Random();
@@ -32,9 +23,9 @@ class Phoggle
         List<List<string>> theBoardFullOfDice = MakeABoard();
 
         //create the words to guess list
-        for (var index = 0; index < gameWordsToGuessList.Count; index++)
+        for (var index = 0; index < _gameWordsToGuessList.Count; index++)
 		{
-			Console.WriteLine(gameWordsToGuessList[index]);
+			Console.WriteLine(_gameWordsToGuessList[index]);
 		}
 		
 		//print the game board
@@ -63,11 +54,11 @@ class Phoggle
 		Console.WriteLine(" ");
 
 
-		while (isTheGameBeingPlayed) //is the game being played? yes, yes it is.
+		while (_isTheGameBeingPlayed) //is the game being played? yes, yes it is.
 		{
 			string thisString = Console.ReadLine();
 			string thatString = thisString.ToUpper();
-			playerInputList.Add(thatString);
+			_playerInputList.Add(thatString);
 		}
 
 	}
@@ -79,7 +70,7 @@ class Phoggle
         List<string> row = new List<string>();
         for (var index = 0; index < 5; index++)
         {
-            row.Add(aDie[rnd.Next(6)]);
+            row.Add(_aDie[rnd.Next(6)]);
         }
         return row;
     }
@@ -111,25 +102,25 @@ class Phoggle
 		aTimer.Stop();
 		aTimer.Dispose();
 		Console.WriteLine("Timer has expired. Terminating the application...");
-		isTheGameBeingPlayed = false;
+		_isTheGameBeingPlayed = false;
 
 		Console.WriteLine("");
 		Console.WriteLine("The words that you submitted were.");
 		Console.WriteLine("");
 
-		for (var index = 0; index < playerInputList.Count; index++)
+		for (var index = 0; index < _playerInputList.Count; index++)
 		{
-			Console.WriteLine(playerInputList[index]);
+			Console.WriteLine(_playerInputList[index]);
 		}
 
 		Console.WriteLine("");
 		Console.WriteLine("Are the submitted words present in the guess list?");
 		Console.WriteLine("");
 
-		//for each item in the playerInputList, check if the item is contained within the gameWordsToGuessList
-		foreach (var aGuess in playerInputList)
+		//for each item in the _playerInputList, check if the item is contained within the _gameWordsToGuessList
+		foreach (var aGuess in _playerInputList)
 		{
-			var isTheGuessCorrect = gameWordsToGuessList.Contains(aGuess).ToString();
+			var isTheGuessCorrect = _gameWordsToGuessList.Contains(aGuess).ToString();
 			Console.WriteLine(isTheGuessCorrect);
 		}
 
@@ -137,57 +128,19 @@ class Phoggle
 		Console.WriteLine("Does the submitted word receive a score?");
 		Console.WriteLine("");
 
-		foreach (var scorableGuess in playerInputList)
+		foreach (var scorableGuess in _playerInputList)
 		{
 			
-			var isTheGuessAScore = gameWordScoreList.Contains(scorableGuess);
+			var isTheGuessAScore = _gameWordScoreList.Contains(scorableGuess);
             if (isTheGuessAScore == true)
 			{
-                gameWordScoreList.Remove(scorableGuess);
-				playerScore += 1;
+                _gameWordScoreList.Remove(scorableGuess);
+				_playerScore += 1;
             }
 			Console.WriteLine(isTheGuessAScore);
 		}
 		Console.WriteLine("");
-		Console.WriteLine("The player's score is " + playerScore);
+		Console.WriteLine("The player's score is " + _playerScore);
 		Console.WriteLine("");
 	}
-
-
-	private static void PrintAssignmentByValueVsByReferenceExample()
-	{
-		// assignment by value
-		Console.WriteLine("Begin assignment by value example");
-		var baseString = "bla";
-		Console.WriteLine($"baseString is {baseString}");
-
-		var stringAssignment = baseString;
-		Console.WriteLine($"stringAssignment is {stringAssignment}");
-		Console.WriteLine($"baseString is {baseString}");
-
-		stringAssignment = "doot";
-		Console.WriteLine($"reassigned to doot, stringAssignment is now {stringAssignment}");
-		Console.WriteLine($"baseString is {baseString}");
-		Console.WriteLine("End assignment by value example");
-		// end assignment by value
-
-		// assignment by reference
-		Console.WriteLine("Begin assignment by reference example");
-		var baseList = new List<string> { "bla" };
-		Console.WriteLine($"baseList is {string.Join(" ", baseList)}");
-
-		var listAssignment = baseList;
-		Console.WriteLine($"listAssignment is {string.Join(" ", listAssignment)}");
-		Console.WriteLine($"baseList is {string.Join(" ", baseList)}");
-
-		listAssignment.Remove("bla");
-		Console.WriteLine($"removed bla listAssignment is {string.Join(" ", listAssignment)}");
-		Console.WriteLine($"baseList is {string.Join(" ", baseList)}");
-		baseList.Add("doot");
-		Console.WriteLine($"added doot listAssignment is {string.Join(" ", listAssignment)}");
-		Console.WriteLine($"added doot baseList is {string.Join(" ", baseList)}");
-		Console.WriteLine("End assignment by reference example");
-		// end assignment by reference
-	}
-		
 }
